c87fcc3ecc6200a6f4661d640b56021e12d90de2
Gabriel Abreu <gabrielabreualves@gmail.com>
2023-04-02 01:42:29 -0300

    alterações na estrutura padrão do projeto

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Icon from 'svelte-icons-pack/Icon.svelte';
   	import AiOutlineNodeExpand from 'svelte-icons-pack/ai/AiOutlineNodeExpand';
 </script>
@@ -15,33 +12,22 @@
 
 	<Icon src={AiOutlineNodeExpand} />
 
-	<!-- <span class="welcome">
-		<picture>
-			<source srcset={welcome} type="image/webp" />
-			<img src={welcome_fallback} alt="Welcome" />
-		</picture>
-	</span> -->
-
-
-	
-	<!-- <Counter /> -->
-
 	<h1 class="text-[32px] text-sky-500 mb-10">
 		Sistema de Controle de Gastos
 	</h1>
 
-	<div class="flex flex-col gap-4 mb-10">
+	<div class="flex justify-center gap-4 mb-10">
 		<a href="/entrada">
-			<button type="button" class="bg-yellow-500 button-home text-sky-50 p-2 rounded-md flex justify-center gap-2">
+			<button type="button" class="bg-yellow-500 button-home text-sky-50 p-2 rounded-md flex justify-center items-center gap-2">
 				<Icon src={AiOutlineNodeExpand} />
-				Cadastrar Nova Entrada
+				Entradas
 			</button>
 		</a>
 
 		<a href="/saida">
-			<button type="button" class="bg-yellow-500 button-home text-sky-50 p-2 rounded-md flex justify-center gap-2">
+			<button type="button" class="bg-yellow-500 button-home text-sky-50 p-2 rounded-md flex justify-center items-center gap-2">
 				<Icon src={AiOutlineNodeExpand} />
-				Cadastrar Nova Saída
+				Saídas
 			</button>
 		</a>										
 	</div>
@@ -78,20 +64,4 @@
 		align-items: center;
 		flex: 0.6;
 	}
-
-	/* .welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	} */
 </style>
--- src/routes/saida/+page.svelte
+++ src/routes/saida/+page.svelte
@@ -1,3 +1,3 @@
 <section class="text-[22px]">
-    <p> Teste </p>
+    <p> Saída </p>
 </section>
