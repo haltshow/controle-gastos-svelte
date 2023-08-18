b60392bdce8f91d9f44dfed270ae6c989083def6
Gabriel Abreu <gabrielabreualves@gmail.com>
2023-08-18 20:22:42 -0300

    teste

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,10 +4,10 @@ import bcrypt from 'bcrypt';
 import prisma from "$lib/server/prisma";
 
 export const load: PageServerLoad = async ({ locals }) => {
-    if (locals.user) {
-        console.log("locals login: ", locals)
-        throw redirect(307, '/')
-    }
+    // if (locals.user) {
+    //     console.log("locals login: ", locals)
+    //     throw redirect(307, '/')
+    // }
 }
 
 const login: Action = async ({ cookies, request }) => {
