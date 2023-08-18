94ae28c35b9bb6920cbaf1f3ec75f68e04a12f00
Gabriel Abreu <gabrielabreualves@gmail.com>
2023-08-18 19:47:32 -0300

    modificando locals svelte

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -19,6 +19,12 @@ export const handle: Handle = async ({ event, resolve}) => {
             name: user.name,
             username: user.username
         }
+    } else {
+        event.locals.user = {
+            id: '',
+            name: '',
+            username: ''
+        }
     }
 
     return await resolve(event)
